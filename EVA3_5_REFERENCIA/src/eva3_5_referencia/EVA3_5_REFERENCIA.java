ed920ec07e291bd637dc120a5bc0101e55283db6
ihdzg <143562753+ihdzg@users.noreply.github.com>
2023-11-24 17:51:19 -0600

    Add files via upload

---
+++ EVA3_5_REFERENCIA/src/eva3_5_referencia/EVA3_5_REFERENCIA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_5_referencia;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA3_5_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+    }
+    
+}
